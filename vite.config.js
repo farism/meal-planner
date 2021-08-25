ba67e439c4ee019a5fad7dd0efe5bff99b7be4fc
Faris Mustafa <faris.mustafa@procore.com>
2021-08-25 01:16:39 -0700

    vite config for prod

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
+export default defineConfig(({ command, mode }) => {
+  return {
+    base: mode === 'production' ? '/meal-planner/' : '/',
+    plugins: [svelte()],
+  }
 })
